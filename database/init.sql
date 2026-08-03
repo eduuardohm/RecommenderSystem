a4db053396514b6c2ca800cf0820b2f604ec6044
Eduardo <marquesaedu@gmail.com>
2026-08-02 23:03:31 -0300

    feat: criando esquema do banco e preparação para o pipeline ETL

---
+++ database/init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY
+);
+
+CREATE TABLE "movies" (
+  "id" integer PRIMARY KEY,
+  "title" varchar(255) NOT NULL
+);
+
+CREATE TABLE "genres" (
+  "id" integer PRIMARY KEY,
+  "name" varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE "movie_genres" (
+  "movie_id" integer NOT NULL,
+  "genre_id" integer NOT NULL,
+  PRIMARY KEY ("movie_id", "genre_id")
+);
+
+CREATE TABLE "ratings" (
+  "user_id" integer NOT NULL,
+  "movie_id" integer NOT NULL,
+  "rating" real NOT NULL,
+  "rated_at" timestamp NOT NULL,
+  PRIMARY KEY ("user_id", "movie_id")
+);
+
+ALTER TABLE "ratings" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "ratings" ADD FOREIGN KEY ("movie_id") REFERENCES "movies" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "movie_genres" ADD FOREIGN KEY ("movie_id") REFERENCES "movies" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "movie_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("id") DEFERRABLE INITIALLY IMMEDIATE;
